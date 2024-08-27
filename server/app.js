f1e1be362c82ff56111bc87f0a34567937238f6f
mumer17865 <116781030+mumer17865@users.noreply.github.com>
2024-08-27 14:58:51 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -11,7 +11,13 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: [""],
+    method: ["POST", "GET"],
+    credentials: true,
+  }
+));
 
 app.use('/auth', require('./routes/authRoutes'));
 app.use('/products', require('./routes/productRoutes'));
